342828cfc1356fd2e5c3de45a899b082151cb5ee
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-08-22 19:35:32 -0300

    Project Structure

---
+++ src/main/java/br/com/rocha/JavaBackend/JavaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.rocha.JavaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendApplication.class, args);
+	}
+
+}
